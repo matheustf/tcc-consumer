74acfaf206e54cba4d71df55387b78916091562f
Matheus Teles <teles.matheus@hotmail.com>
2018-08-09 10:20:36 -0300

    zull e correcao do eureka

--- src/main/java/com/puc/tcc/consumer/ConsumerApplication.java
+++ src/main/java/com/puc/tcc/consumer/ConsumerApplication.java
@@ -3,8 +3,10 @@ package com.puc.tcc.consumer;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableRabbit
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConsumerApplication {
 
